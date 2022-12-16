6e516038626222665613efa8600f2a790a73a578
Micky373 <michaeltamirie@gmail.com>
2022-12-16 13:25:17 +0300

    Finalize the backend

---
+++ api/main.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI, File, UploadFile
+import uvicorn
+import numpy as np
+import tensorflow as tf
+from PIL import Image
+from io import BytesIO
+import cv2
+
+
+app = FastAPI()
+
+MODEL = tf.keras.models.load_model('../saved_models/1')
+
+CLASS_NAMES = ['Early Blight', 'Late Blight', 'Healthy']
+
+def read_file_image(data):
+    image = np.array(Image.open(BytesIO(data)))
+    return image
+
+@app.post('/predict')
+async def predict(
+    file: UploadFile = File(...)
+): 
+    image = read_file_image(await file.read())
+    image_batch = np.expand_dims(image,0)
+    
+    prediction = MODEL.predict(image_batch)
+    
+    predicted_class = CLASS_NAMES[np.argmax(prediction[0])]
+    confidence = np.max(prediction[0])
+    
+    return {
+        'class': predicted_class,
+        'confidence':float(confidence)
+    }
+    
+@app.get('/ping')
+async def ping():
+    return "Hey Micky how you doing!!!"
+
+if __name__ == "__main__":
+    uvicorn.run('main:app',host='localhost',port=8000,reload=True)
